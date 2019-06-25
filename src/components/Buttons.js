094e15927bdd900f54b9cd0d524898bf9e34b948
endriu <aolaszewski@gmail.com>
2019-06-26 00:38:17 +0200

    fix styles button color

--- src/components/Buttons.js
+++ src/components/Buttons.js
@@ -8,14 +8,23 @@ const Buttons = props => {
         display: !props.play || props.winner ? 'none' : 'flex'
       }}>
       <div className="game-buttons__play">
-        <i onClick={e => props.playGame(e.target.id)} id={'paper'} className="fas fa-hand-paper fa-10x">
-        </i>
-        <i onClick={e => props.playGame(e.target.id)} id={'rock'} className="fas fa-hand-rock fa-10x">
-        </i>
-        <i onClick={e => props.playGame(e.target.id)} id={'scissors'} className="fas fa-hand-scissors fa-10x">
-        </i>
+        <i
+          onClick={e => props.playGame(e.target.id)}
+          id={'paper'}
+          className="fas fa-hand-paper fa-10x"
+        />
+        <i
+          onClick={e => props.playGame(e.target.id)}
+          id={'rock'}
+          className="fas fa-hand-rock fa-10x"
+        />
+        <i
+          onClick={e => props.playGame(e.target.id)}
+          id={'scissors'}
+          className="fas fa-hand-scissors fa-10x"
+        />
       </div>
-      <h1>Make a choose!</h1>
+      <h1 className="choose-info">Make a choose!</h1>
     </div>
   );
 };
